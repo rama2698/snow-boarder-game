22532a1cefb2864655a6df607c3c4592deadeedb
rama2698 <r.bhagat2698@gmail.com>
2024-02-05 22:35:28 +0530

    play the sound only once if player is crashed

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -8,8 +8,10 @@ public class CrashDetector : MonoBehaviour
     [SerializeField] float delayTimer = 0.5f;
     [SerializeField] ParticleSystem crashEffect;
     [SerializeField] AudioClip crashSFX;
+    bool hasCrashed;
     void OnTriggerEnter2D(Collider2D other){
-        if(other.tag == "Ground"){
+        if(other.tag == "Ground" && !hasCrashed){
+            hasCrashed = true;
             crashEffect.Play();
             FindObjectOfType<PlayerController>().disableMove();
             GetComponent<AudioSource>().PlayOneShot(crashSFX);
